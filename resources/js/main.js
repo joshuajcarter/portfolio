a751f8669a79344445a60d761316aea06f6a632f
Squashies <19284840+joshuajcarter@users.noreply.github.com>
2024-06-17 19:14:00 +0100

    modify main.js DOM handling, modify html files nav bar id and related css

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,30 +1,40 @@
 window.addEventListener("load", () => {
   ["#hero-make", "#hero-ideas", "#hero-happen"].forEach((id) => {
-    document.querySelector(id).style.opacity = "1";
+    const element = document.querySelector(id);
+    if (element) {
+      element.style.opacity = "1";
+    }
   });
-});
 
-const menuButton = document.getElementById("menu-button");
-const closeMenu = document.getElementById("close-menu-button")
-const workLink = document.getElementById('work-link')
-const dropDown = document.getElementById('nav-dropdown')
-const letsTalk = document.querySelectorAll('.talk')
+  const menuButton = document.getElementById("menu-button");
+  const closeMenu = document.getElementById("close-menu-button");
+  const workLink = document.getElementById("work-link");
+  const dropDown = document.getElementById("nav-dropdown");
+  const letsTalk = document.querySelectorAll(".talk");
 
-function showMenu() {
-  menuButton.classList.add("hide");
-  closeMenu.classList.remove("hide");
-  dropDown.classList.remove("hide");
-}
+  console.log("menuButton:", menuButton);
+  console.log("closeMenu:", closeMenu);
+  console.log("workLink:", workLink);
+  console.log("dropDown:", dropDown);
+  console.log("letsTalk:", letsTalk);
 
-function hideMenu() {
-  closeMenu.classList.add("hide");
-  menuButton.classList.remove("hide");
-  dropDown.classList.add("hide");
-}
-menuButton.addEventListener('click', showMenu)
-closeMenu.addEventListener('click', hideMenu)
-workLink.addEventListener("click", hideMenu);
+  function showMenu() {
+    if (menuButton) menuButton.classList.add("hide");
+    if (closeMenu) closeMenu.classList.remove("hide");
+    if (dropDown) dropDown.classList.remove("hide");
+  }
 
-letsTalk.forEach((button) => {
-  button.addEventListener("click", showMenu);
+  function hideMenu() {
+    if (closeMenu) closeMenu.classList.add("hide");
+    if (menuButton) menuButton.classList.remove("hide");
+    if (dropDown) dropDown.classList.add("hide");
+  }
+
+  if (menuButton) menuButton.addEventListener("click", showMenu);
+  if (closeMenu) closeMenu.addEventListener("click", hideMenu);
+  if (workLink) workLink.addEventListener("click", hideMenu);
+
+  letsTalk.forEach((button) => {
+    button.addEventListener("click", showMenu);
+  });
 });
