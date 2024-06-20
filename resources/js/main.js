b76ffaf6211d184a807b8a7df521d213663f38e5
Squashies <19284840+joshuajcarter@users.noreply.github.com>
2024-06-20 20:20:56 +0100

    modify project.html style .image-medium border radius, implement main.js func reorderCards

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,12 +11,14 @@ window.addEventListener("load", () => {
   const workLink = document.getElementById("work-link");
   const dropDown = document.getElementById("nav-dropdown");
   const letsTalk = document.querySelectorAll(".talk");
+  const caseCards = document.querySelectorAll(".case-study-card")
 
   console.log("menuButton:", menuButton);
   console.log("closeMenu:", closeMenu);
   console.log("workLink:", workLink);
   console.log("dropDown:", dropDown);
   console.log("letsTalk:", letsTalk);
+  console.log("caseCards:", caseCards)
 
   function showMenu() {
     if (menuButton) menuButton.classList.add("hide");
@@ -30,6 +32,22 @@ window.addEventListener("load", () => {
     if (dropDown) dropDown.classList.add("hide");
   }
 
+  function reorderCards() {
+    caseCards.forEach((card, index) => {
+      const imgFrame = card.querySelector(".img-frame");
+      const textFrame = card.querySelector(".case-text-frame");
+      if (index % 2 == 0 && window.innerWidth >= 600) {
+        if (textFrame.nextElementSibling !== imgFrame) {
+          card.insertBefore(textFrame, imgFrame);
+        }
+      } else {
+        if (imgFrame.nextElementSibling !== textFrame) {
+          card.insertBefore(imgFrame, textFrame);
+        }
+      }
+    });
+  }
+
   if (menuButton) menuButton.addEventListener("click", showMenu);
   if (closeMenu) closeMenu.addEventListener("click", hideMenu);
   if (workLink) workLink.addEventListener("click", hideMenu);
@@ -37,4 +55,7 @@ window.addEventListener("load", () => {
   letsTalk.forEach((button) => {
     button.addEventListener("click", showMenu);
   });
+
+  reorderCards();
+  window.addEventListener("resize", reorderCards);
 });
